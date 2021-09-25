10eec13aa6f6c7cecfa0691875fc69c6bcfb0ab2
Shweta2105 <sghadi21@gmail.com>
2021-09-25 11:49:14 +0530

    background process added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:local_notification/localNotification.dart';
+import 'package:local_notification/notificationInitializer.dart';
+import 'package:workmanager/workmanager.dart';
+
+void callbackDispatcher() {
+  Workmanager().executeTask((taskName, inputData) {
+    //show the notification
+    NotificationPlugin.Initializer();
+    NotificationPlugin.ShowOneTimeNotification(DateTime.now());
+    return Future.value(true);
+  });
+}
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher);
+  Workmanager().registerPeriodicTask("test_workertask", "test_workertask",
+      frequency: Duration(
+        minutes: 15,
+      ),
+      initialDelay: Duration(minutes: 1),
+      inputData: {"data1": "value1", "data2": "value2"});
   runApp(MyApp());
 }
 
